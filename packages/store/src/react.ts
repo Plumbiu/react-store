a0c0cec7e412580329df266334a92993f1abb969
Plumbiu <3434909403@qq.com>
2024-09-03 13:16:44 +0800

    fix: type

--- packages/store/src/react.ts
+++ packages/store/src/react.ts
@@ -1,20 +1,73 @@
 /* eslint-disable @stylistic/semi-style */
 /* eslint-disable @stylistic/indent */
 import { Draft, produce } from 'immer'
-import { Plugin, BaseState } from './types'
-import { createStoreFactory } from './factory'
+import { useSyncExternalStore } from 'react'
+import { Plugin, BaseState, Listener } from './types'
+import { isEqual } from './utils'
+
+type $SetParams<T extends BaseState> = (draft: Draft<T> | T) => void
+
+export function createStoreFactory<T extends BaseState>(
+  state: T,
+  produce: (state: T, param: $SetParams<typeof state>) => T,
+  plugin?: Plugin<T>,
+) {
+  const listeners = new Set<Listener>()
+  ;(state as any).$set = (param: $SetParams<T>) => {
+    const nextState = produce(state, param)
+    if (
+      isEqual(nextState, state) ||
+      plugin?.propsAreEqual?.(state, nextState)
+    ) {
+      return
+    }
+    state = nextState
+    if (plugin?.shouldUpdate && !plugin.shouldUpdate(nextState)) {
+      return
+    }
+    listeners.forEach((fn) => fn())
+    plugin?.afterUpdate?.(nextState)
+  }
+  plugin?.setup?.(state, plugin)
+
+  for (const key in state) {
+    let fn = state[key]
+    if (typeof fn === 'function') {
+      ;(state[key] as any) = (...args: any[]) => fn.apply(state, args)
+    }
+  }
+
+  function returnFn(): T
+  function returnFn<K extends keyof T>(selector: K): T[K]
+  function returnFn<K extends keyof T>(
+    selector?: K,
+    getServerSnapshot?: () => T,
+  ) {
+    const data = useSyncExternalStore(
+      (listener) => {
+        listeners.add(listener)
+        return () => listeners.delete(listener)
+      },
+      () => (selector ? state[selector] : state),
+      getServerSnapshot,
+    )
+    return data
+  }
+
+  return returnFn
+}
 
 export const createStore = <T extends BaseState>(
   _state: T & ThisType<T & { $set: (state: Partial<T>) => void }>,
-  plugin?: Plugin<typeof _state>,
+  plugin?: Plugin<any>,
 ) =>
-  createStoreFactory(
+  createStoreFactory<T>(
     _state,
     (state, param) => Object.assign({}, state, param),
     plugin,
   )
 
-export const createImmerStore = <T extends BaseState>(
+export const createImmerStore = <T extends BaseState, P = Plugin<T>>(
   _state: T & ThisType<T & { $set: (cb: (draft: Draft<T>) => void) => void }>,
-  plugin?: Plugin<typeof _state>,
-) => createStoreFactory(_state, produce, plugin as any)
+  plugin?: Plugin<any>,
+) => createStoreFactory<T>(_state, produce, plugin)
--- packages/store/src/types.ts
+++ packages/store/src/types.ts
@@ -1,4 +1,3 @@
-import { Draft } from 'immer'
 import { useSyncExternalStore } from 'react'
 
 type SyncExternalStoreParams = Parameters<typeof useSyncExternalStore>
@@ -6,14 +5,7 @@ export type Listener = Parameters<SyncExternalStoreParams[0]>[0]
 export interface BaseState {
   [key: string | number | symbol]: any
 }
-export type State<T extends BaseState> = T &
-  ThisType<
-    T & {
-      $set: (
-        state: Partial<T>,
-      ) => void | ((cb: (draft: Draft<T>) => void) => void)
-    }
-  >
+
 export interface Plugin<T extends BaseState> {
   setup?: (state: T, plugin?: Plugin<T>) => void
   propsAreEqual?: (prevState: T, nextState: T) => boolean
